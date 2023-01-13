1989038d0798dcaaf5c6d734471ac7d178980cbf
Dohyeon Kwon <playdev7@gmail.com>
2023-01-13 18:02:27 +0900

    js 천천히 진행중

--- study/js/07/js/quiz-1.js
+++ study/js/07/js/quiz-1.js
@@ -1,4 +1,5 @@
 		var numbers = [2, 4, 6, 8, 10];
+		addResult(numbers); // addResult 함수 실행
 		showArray(numbers);
 
 		function showArray(arr) {
@@ -8,4 +9,14 @@
 			}
 			str += "</tr></table>";
 			document.write(str);
+		}
+
+		// 배열의 모든 정수를 더해서 끝에 추가해주는 addResult 함수 선언 및 정의.
+		function addResult(arr) {
+			var result = 0;
+			for(i=0; i<arr.length; i++) {	// 배열의 길이만큼 반복.
+				
+				result += parseInt(arr[i]);		// 배열 요소 정수 아닌줄 알고 parseInt() 사용.. :)
+			}
+			arr.push(result);
 		}
--- study/js/08/js/domevent.js
+++ study/js/08/js/domevent.js
@@ -1,5 +1,6 @@
     var pic = document.querySelector('#pic');
-    
+    pic.addEventListener("mouseover", changePic, false); 
+
 		function changePic() {			
 			pic.src = "images/boy.png";
     }
