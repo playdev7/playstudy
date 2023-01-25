2b56d060ab73e73bd17d48f1a66db9dd9ffa0518
Dohyeon Kwon <playdev7@gmail.com>
2023-01-25 21:50:55 +0900

    html 부터 사랑하자 안해봤던 것처럼

--- study/js/09/js/order.js
+++ study/js/09/js/order.js
@@ -1,18 +1,31 @@
-var check = document.querySelector("#shippingInfo"); // 체크 상자 요소를 가져와 check 변수에 저장.
+var check = document.querySelector("#shippingInfo"); // #shippingInfo 인  checkbox 요소를 check 변수로 저장.
 
-check.addEventListener("click", function() {    // 체크 상자 클릭 이벤트 팔생 시 함수 실행.
-    if(check.checked == true) { // 체크상자가 체크되어 있다면
-        document.querySelector("#shippingName").value = document.querySelector("#billingName").value;
-        //  배송정보 이름의 값을 주문정보 이름의 값으로
-        document.querySelector("#shippingTel").value = document.querySelector("#billingTel").value;
-        //  배송정보 연락처의 값을 주문정보 연락처 값으로.
-        document.querySelector("#shippingAddr").value = document.querySelector("#billingAddr").value;
-        //  배송정보 주소의 값을 주문정보 주소의 값으로.
+check.addEventListener("click", function(){   //  check에 대한 addEventListener() 사용 및 정의
+    // addEventListener() 함수를 통해 특정 이벤트 발생 시 처리할 내용을 정의하게 됨.
+
+    // 체크박스를 클릭 했을 때, 체크 여부에 따라 다르게 작동하도록 구현.
+    if(check.checked == true) {        // check의 checked 상태가 참 일때
+        document.querySelector("#shippingName").value      // #shippingName 의 값을 #billingName으로.
+        = document.querySelector("#billingName").value;
+
+        document.querySelector("#shippingTel").value   // #shippingTel 의 값을 #billingTel 으로.
+        = document.querySelector("#billingTel").value;
+
+        document.querySelector("#shippingAddr").value   // #shippingAddr 의 값을 #billingAddr 으로.
+        = document.querySelector("#billingAddr").value;
     }
-    else{   // 체크 상자의 체크가 해제된다면.
-        // shipping 배송 정보 관련 텍스트 필드를 빈 값으로 설정.
-        document.querySelector("#shippingName").value = "";
-        document.querySelector("#shippingTel").value = "";
-        document.querySelector("#shippingAddr").value = "";
+
+    else{      // check를 클릭해서 체크가 해제되었을때.
+        document.querySelector("#shippingName").value = "";        // #shippingName 의 값을 null로 초기화.
+        
+        document.querySelector("#shippingTel").value = "";     // #shippingTel 의 값을 null로 초기화.
+        
+        document.querySelector("#shippingAddr").value = "";     // #shippingAddr 의 값을 null로 초기화.
+        
+        // 개념복기 - undefined 와 null의 차이점.
+        // undefined 는 아직 값과 자료형이 정의되지 않은 상태의 변수.
+        // null은 한 번 값이 정의되었다가, 없는 값으로 재정의 된 상태의 변수.
+        // typeof 변수명 구문을 통해 자료형을 확인할 수 있다.
+        // 핵심: JS는 undefined와 null을 구분한다.
     }
 });
