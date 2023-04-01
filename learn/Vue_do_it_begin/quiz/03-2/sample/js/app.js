b26f1d4f3052d3a44ca2b21c201e67a2337a7458
Dohyeon Kwon <playdev7@gmail.com>
2023-04-01 16:43:38 +0900

    Vue.js 입문 - 지역 컴포넌트 실습

---
+++ learn/Vue_do_it_begin/quiz/03-2/sample/js/app.js
@@ -0,0 +1,23 @@
+// 실습 #1 - `todo-footer` 컴포넌트 전역 등록
+Vue.component('todo-footer', {
+  template: '<p>This is another child global component</p>'
+});
+// <p>This is another child global component</p> 를 template 으로 갖는 컴포넌트를 등록해보세요.
+
+// 실습 #2 관련 - 'todo-list' 컴포넌트 템플리 구현
+var template = {
+  template : '<p>This is another child local component</p>'
+};
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    message : 'This is a parent component'
+  },
+
+  // 실습 #2 - `todo-list` 컴포넌트 지역 등록
+  components : {
+    'todo-list' : template
+  }
+  // <p>This is another child local component</p> 를 template 으로 갖는 컴포넌트를 등록해보세요.
+});
